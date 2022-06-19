b13b816f7acfb77961896f6e5dd35b555ddd57b1
unknown <egkirpichev@gmail.com>
2022-06-20 00:44:32 +0300

    test(trello_05): add fake data for drag and drop functionality testing

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,17 @@
 import { DigitalClock } from "./components/Сlock.js";
 import { enableDrag } from "./components/Column.js";
+import {Card} from "./components/Card.js"
 
 async function initApp() {
   const clock = new DigitalClock();
   clock.start();
+
+	const card1 = new Card ("", "title", "user", "description", "column-todo");
+	card1.render()
+	const card2 = new Card ("", "title", "user", "description", "column-todo");
+	card2.render()
+	const card3 = new Card ("", "title", "user", "description", "column-todo");
+	card3.render()
   
   enableDrag();
 }

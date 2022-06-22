b3948897654377b88730d57ebb18cebca3d7339f
unknown <egkirpichev@gmail.com>
2022-06-22 20:47:59 +0300

    refactor(trello_15): refactor earning modal HTML and styles

--- src/scripts/components/WarningModal.js
+++ src/scripts/components/WarningModal.js
@@ -1,35 +1,35 @@
 
-function enableWarningModal() {
-	const modal = document.querySelector('#modal-warning');
-	const message = modal.querySelector('#warning-message');
-	const confirmBtn = modal.querySelector('[data-action="confirm"]');
-	const cancelBtn = modal.querySelector('[data-action="cancel"]');
+function CreateWarningModal() {
+	this.modal = document.querySelector('#modal-warning');
+	this.message = this.modal.querySelector('#warning-message');
+	this.confirmBtn = this.modal.querySelector('[data-action="confirm"]');
+	this.cancelBtn = this.modal.querySelector('[data-action="cancel"]');
 
 
-	confirmBtn.addEventListener('click', () => {
+	this.confirmBtn.addEventListener('click', () => {
 		modal.close()
 		message.textContent = ""
 		return "confirmed"
 	});
 
-	cancelBtn.addEventListener('click', () => {
+	this.cancelBtn.addEventListener('click', () => {
 		modal.close()
 		message.textContent = ""
 		return "canceled"
 	});
+
+	this.popWarningModal = (event) => {
+		const modal = document.querySelector('#modal-warning');
+		const message = modal.querySelector('#warning-message');
+
+		if (event.target.dragged === "true") {
+			message.textContent = "Please, complete current tasks before starting new ones!"
+			modal.openModal()
+		} else if (event.target.id === "btn-delete-all") {
+			modal.openModal()
+		}	
+	}
 }
 
-function popWarningModal(event) {
-	const modal = document.querySelector('#modal-warning');
-	const message = modal.querySelector('#warning-message');
 
-	if (event.target.dragged === "true") {
-		message.textContent = "Please, complete current tasks before starting new ones!"
-		modal.openModal()
-	} else if (event.target.id === "btn-delete-all") {
-		modal.openModal()
-	}	
-}
-
-
-export {enableWarningModal, popWarningModal}
+export {CreateWarningModal}
